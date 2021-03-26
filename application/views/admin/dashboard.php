ba1e29ed5e597f05b8c65834038b675f2f8e927f
Rexter Dayuta <rexterdayuta@gmail.com>
2021-03-27 02:57:17 +0800

    label update

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,4 +1,4 @@
-<title>Admin - COS ePoster Submission Site</title>
+<title>Admin - COS Exhibitor Graphics Site</title>
 
 <main role="main" style="margin-top: 70px;margin-left: 20px;margin-right: 20px;">
     <div class="row">
